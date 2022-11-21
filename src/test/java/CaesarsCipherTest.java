ab321604d948dc068fad06eb0f6da2f5fa908a4e
Zu Hong Goh <zuhong.goh@adnovum.sg>
2022-11-21 22:57:48 +0800

    initial commit

---
+++ src/test/java/CaesarsCipherTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarsCipherTest {
+    private CaesarsCipher caesarsCipher = new CaesarsCipher();
+
+    @Test
+    void testCipheredMessageWIthOffsetOf12(){
+        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 12));
+    }
+
+    @Test
+    void testEmptyString(){
+        assertEquals("", caesarsCipher.cipher("",12));
+    }
+}
